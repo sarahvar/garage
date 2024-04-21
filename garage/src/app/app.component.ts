9c263bd2987ae6d807700be1eb9f94482f634ba5
sarahvar <sarahvarin95@gmail.com>
2024-04-21 23:49:06 +0200

    config

--- garage/src/app/app.component.ts
+++ garage/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { GarageService } from './garage.service';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers:[GarageService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'garage';
+  cars: Object
+  constructor(private garageService:GarageService){}
+
+  ngOnInit(){
+      console.log("On init......")
+      this.garageService.getCars().subscribe(datas => {
+        this.cars = datas
+      })
+  }
 }
