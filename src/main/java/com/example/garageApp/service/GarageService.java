c7bdd117b606a589c6030e5cb9e9e6536ce0ee0f
sarahvar <sarahvarin95@gmail.com>
2024-05-11 23:42:16 +0200

    login_java

---
+++ src/main/java/com/example/garageApp/service/GarageService.java
@@ -0,0 +1,20 @@
+
+package com.example.garageApp.service;
+
+import java.util.List;
+
+import com.example.garageApp.model.Car;
+
+public interface GarageService {
+    
+    List<Car> getAllCars();
+
+    Car getCarById(Long id);
+
+    Car add (Car car);
+
+    void deleteCar(Long id);
+    
+    Car updateCar(Long id, Car updatedCar);
+
+}
